f679acb21b4a1864407290893e62c3ae36c2dbd4
Louis MAZEAUD <lmazeaud@e3r9p11.42.fr>
2018-07-17 18:50:17 +0200

    Create hearder file for parser's function

---
+++ includes/ft_utils_parser.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils_parser.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmazeaud <lmazeaud@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/17 18:46:22 by lmazeaud          #+#    #+#             */
+/*   Updated: 2018/07/17 18:46:32 by lmazeaud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_UTILS_PARSER_H
+# define FT_UTILS_PARSER_H
+
+# include "ft_printf.h" 
+
+char	*ft_find_regex(char **src);
+int	    ft_get_regex_len(char **src);
+int		ft_parser_get_width(char *reg);
+int		ft_parser_get_precision(char *regex);
+t_data	*ft_parser_flags_checker(t_data *elem);
+t_data	*ft_parser_option_checker(t_data *elem)
+int		ft_parser_get_zero_flag_width(char *reg);
+
+#endif
+
