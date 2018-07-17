075380f6df59234d69fb969d98aa3139b82c90c3
Louis MAZEAUD <lmazeaud@e3r9p11.42.fr>
2018-07-17 21:37:35 +0200

    create new header for utils and stuct

---
+++ includes/ft_struct.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_struct.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmazeaud <lmazeaud@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/17 20:32:24 by lmazeaud          #+#    #+#             */
+/*   Updated: 2018/07/17 20:35:32 by lmazeaud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STRUCT_H
+# define FT_STRUCT_H
+
+typedef struct	s_flags
+{
+	int			zero;
+	int			hashtag;
+	int			plus;
+	int			dash;
+	int			spaces;
+	int			length;
+}				t_flags;
+
+typedef struct		s_data
+{
+    char 			*text;				/* le texte avant le % recontré*/
+	t_flags			flags;			    /* le flag */
+	char			*expression; 	    /* l'expression entiere sans le % sign au debut */
+	int				width;			    /* la taille minimale de l'output */
+	char			type;			    /* le type du content */
+	int				precision;		    /* la precision exigée en int */
+	void			*value;			    /* la variable envoyé correspondant au type demandé recupéré depuis va_ap */
+	void			*buffer;		    /* la valeur final retourné */
+	int				buffer_len;		    /* la longeur du buffer a afficher */
+    struct s_data	*next;		        /* la prochaine structure */
+}		            t_data;
+
+#endif
--- includes/ft_utils_list.h
+++ includes/ft_utils_list.h
@@ -1,26 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_utils_parser.h                                  :+:      :+:    :+:   */
+/*   ft_utils_list.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lmazeaud <lmazeaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/07/17 18:46:22 by lmazeaud          #+#    #+#             */
-/*   Updated: 2018/07/17 20:30:45 by lmazeaud         ###   ########.fr       */
+/*   Created: 2018/07/17 20:00:35 by lmazeaud          #+#    #+#             */
+/*   Updated: 2018/07/17 20:28:39 by lmazeaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_UTILS_PARSER_H
-# define FT_UTILS_PARSER_H
+#ifndef FT_UTILS_LIST_H
+# define FT_UTILS_LIST_H
 
 # include "./ft_printf.h" 
 
-char	*ft_parser_find_regex(char **src);
-int	    ft_parser_get_regex_len(char **src);
-int		ft_parser_get_width(char *reg);
-int		ft_parser_get_precision(char *regex);
-t_data	*ft_parser_flags_checker(t_data *elem);
-int		ft_parser_get_zero_flag_width(char *reg);
+void	ft_list_append(t_data **lst, t_data *new);
 
 #endif
-
