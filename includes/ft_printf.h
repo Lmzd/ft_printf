426170f1fe97853bc651cc1e905d31cb1f938fb0
Louis MAZEAUD <lmazeaud@e3r9p11.42.fr>
2018-07-17 21:37:52 +0200

    Update main header ft_printf.h

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: lmazeaud <lmazeaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/09 14:10:31 by lmazeaud          #+#    #+#             */
-/*   Updated: 2018/07/17 18:45:58 by lmazeaud         ###   ########.fr       */
+/*   Updated: 2018/07/17 20:36:11 by lmazeaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,45 +16,13 @@
 # include <stdarg.h>
 # include <stdio.h>
 # include "../libft/includes/libft.h"
-# include "../libft/includes/ft_utils_parser.h"
+# include "ft_struct.h"
 
-# define TYPES "sSpdDioOuUxXcC" /* paul : c'est quoi l'interet de ça ? doublon avec l'enum ? */
+/* UTILS */
+# include "ft_utils_parser.h"
+# include "ft_utils_list.h"
 
-typedef struct s_data	t_data;
-
-enum	e_length
-{
-	null, hh, h, l, ll, j, z
-} 		length;
-
-enum	e_types
-{
-	s, S, p, d, D, i, o, O, u, U, x, X, c, C
-}		types;
-
-typedef struct	s_flags
-{
-	int				zero;
-	int				hashtag;
-	int				plus;
-	int				dash;
-	int				spaces;
-	enum e_length	length;
-}				t_flags;
-
-typedef struct	s_data
-{
-	char 	*text;					/* le texte avant le % recontré*/
-	t_flags			flags;			/* le flag */
-	char			*expression; 	/* l'expression entiere sans le % sign au debut */
-	int				width;			/* la taille minimale de l'output */
-	enum e_types	type;			/* le type du content */
-	int				precision;		/* la precision exigée en int */
-	void			*value;			/* la variable envoyé correspondant au type demandé recupéré depuis va_ap */
-	void			*buffer;		/* la valeur final retourné */
-	int				buffer_len;		/* la longeur du buffer a afficher */
-	t_data			*next;			/* la prochaine structure */
-}				t_data;
+# define TYPES "sSpdDioOuUxXcC"
 
 /*
 ** Parser Function
