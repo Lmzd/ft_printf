00c4f93bc50921b6f9fccb8a3e71b1387526bcdc
Louis MAZEAUD <lmazeaud@e3r13p14.42.fr>
2018-07-18 20:05:57 +0200

    print all sturctures end points

--- srcs/utils/test/ft_test_print_list_elem.c
+++ srcs/utils/test/ft_test_print_list_elem.c
@@ -6,7 +6,7 @@
 /*   By: lmazeaud <lmazeaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/18 12:16:00 by lmazeaud          #+#    #+#             */
-/*   Updated: 2018/07/18 13:51:41 by lmazeaud         ###   ########.fr       */
+/*   Updated: 2018/07/18 19:48:19 by lmazeaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,23 @@
 void    ft_test_print_list_elem(t_data *elem)
 {
     printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-    printf("Type      : %c\n", elem->type);
-    printf("Space     : %d\n", elem->flags.spaces);
-    printf("Hash      : %d\n", elem->flags.hashtag);
-    printf("Zero      : %d\n", elem->flags.zero);
-    printf("Plus      : %d\n", elem->flags.plus);
-    printf("Dash      : %d\n", elem->flags.dash);
-    printf("Width     : %d\n", elem->width);
-    printf("Precision : %d\n", elem->precision);
+    printf("Text    : %s\n", elem->text);
+    printf("Expres  : %s\n", elem->expression);
+    printf("Type    : %c\n", elem->type);
+    printf("Width   : %d\n", elem->width);
+    printf("Precis  : %d\n", elem->precision);
+    printf("            ~~~~~~~~~~~~                  \n");
+    printf("Space   : %d\n", elem->flags.spaces);
+    printf("Hash    : %d\n", elem->flags.hashtag);
+    printf("Zero    : %d\n", elem->flags.zero);
+    printf("Plus    : %d\n", elem->flags.plus);
+    printf("Dash    : %d\n", elem->flags.dash);
+    printf("            ~~~~~~~~~~~~                 \n");
+    printf("h       : %d\n", elem->modifier.h);
+    printf("hh      : %d\n", elem->modifier.hh);
+    printf("l       : %d\n", elem->modifier.l);
+    printf("ll      : %d\n", elem->modifier.ll);
+    printf("j       : %d\n", elem->modifier.j);
+    printf("z       : %d\n", elem->modifier.z);
     printf("______________________________________\n\n");
 }
