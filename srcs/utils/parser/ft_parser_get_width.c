1e2d6ccf0729ab1d75d1936557ee7ac842f2af61
Louis MAZEAUD <lmazeaud@e3r13p14.42.fr>
2018-07-18 20:08:08 +0200

    rethink the get width part using strenght of ft_atoi

--- srcs/utils/parser/ft_parser_get_width.c
+++ srcs/utils/parser/ft_parser_get_width.c
@@ -6,7 +6,7 @@
 /*   By: lmazeaud <lmazeaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/17 18:21:43 by lmazeaud          #+#    #+#             */
-/*   Updated: 2018/07/18 13:58:38 by lmazeaud         ###   ########.fr       */
+/*   Updated: 2018/07/18 18:35:37 by lmazeaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,12 @@
 
 int		ft_parser_get_width(char *reg)
 {
-	char 	*width;
 	int		i;
 	int 	len;
 
-	i = 0;
-	len = 0;
-	while (reg[i++] != '\0')
-		if (reg[i] > 47 && reg[i] < 58)
-			len++;
-	// printf("len : %d\n", len);
-	if (!(width = (char *)malloc(sizeof(char) * len + 1)))
-		return (0);
-	i = 0;	
-	while (reg[i] != '\0')
-	{
-		// printf("%c\n", reg[i]);
-		if (reg[i] > 47 && reg[i] < 58)
-			width[i] = reg[i];
-		i++;
-	}
-	width[i] = '\0';
-	// printf("len : %s\n", width);
-	return (ft_atoi(width));
+    i = 0;
+    while (*reg <= 47 || *reg >= 58)
+        reg++;
+    len = ft_atoi(reg);
+	return (len);
 }
--- srcs/utils/parser/ft_parser_get_zero_flag_width.c
+++ srcs/utils/parser/ft_parser_get_zero_flag_width.c
@@ -6,7 +6,7 @@
 /*   By: lmazeaud <lmazeaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/17 18:15:58 by lmazeaud          #+#    #+#             */
-/*   Updated: 2018/07/17 18:49:11 by lmazeaud         ###   ########.fr       */
+/*   Updated: 2018/07/18 19:10:00 by lmazeaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,16 @@
 
 int		ft_parser_get_zero_flag_width(char *reg)
 {
-	char 	*width;
 	int		i;
 	int 	len;
 
 	i = 0;
 	len = 0;
-	while (*reg != '0')
-		reg++;
-	while (reg[++i] != '\0')
-		if (reg[i] > 47 && reg[i] < 58)
-			len++;
-	if (!(width = (char *)malloc(sizeof(char) * len + 1)))
-		return (0);
-	i = 0;	
-	while (reg[++i] != '\0')
-		if (reg[i] > 47 && reg[i] < 58)
-			width[i - 1] = reg[i];
-	width[len] = '\0';
-	return (ft_atoi(width));
+    while (*reg  != '0')
+        reg++;
+    reg++;
+    while (*reg <= 47 || *reg >= 58)
+        reg++;
+    len = ft_atoi(reg);
+	return (len);
 }
