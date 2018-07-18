426d05d5ad870593f511b5cf5d31adc7e480aad6
Louis MAZEAUD <lmazeaud@e3r13p14.42.fr>
2018-07-18 20:08:43 +0200

    simplify the parser files

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,27 +6,11 @@
 /*   By: lmazeaud <lmazeaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/09 17:58:51 by lmazeaud          #+#    #+#             */
-/*   Updated: 2018/07/18 16:01:23 by lmazeaud         ###   ########.fr       */
+/*   Updated: 2018/07/18 19:18:38 by lmazeaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/ft_printf.h"
-
-/*
-**	Take the regex string store in the struct  && to store
-**			the flag
-**			the width
-**			the precision
-**
-**	each % will as a personnal link
-*/
-
-t_data		*ft_parser_parse_expression(t_data *elem)
-{
-	elem = ft_parser_flags_checker(elem);
-
-	return elem;
-}
 
 /*
 **	Take the all list when we know the type of data recieve
@@ -51,17 +35,17 @@ t_data		*ft_parser_format(const char *format)
 	t_data	*curr;
 
 
-	str = ft_strdup(format);
-    str_mem = str;
 	curr = NULL;
+    str_mem = str;
+	str = ft_strdup(format);
 	begin_list = ft_list_create_data_elem();
 	while (*str)
 	{
 		curr = ft_list_create_data_elem();
 		curr = ft_parser_get_text_and_expression(curr, &str);
-		if (curr->expression)
+		if (curr->expression != NULL)
 		{
-			curr = ft_parser_parse_expression(curr);
+			curr = ft_parser_options_checker(curr);
 		}
         ft_test_print_list_elem(curr);
 		ft_list_append(&begin_list, curr);
