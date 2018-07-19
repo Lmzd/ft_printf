353b0d742e9acc14eb4fd8d4764cd6575f707bde
Your Name <you@example.com>
2018-07-19 16:51:18 +0200

    add format_int compete recipe (bug on previous commit)

--- srcs/format/ft_format_int.c
+++ srcs/format/ft_format_int.c
@@ -3,8 +3,32 @@
 
 void	ft_format_int(t_data *data)
 {
+    char    *str;
+
+    /* 0. setup et error check */
+    if (!data)
+        exit ;
 
     /* 1. d'abord modificateurs de taille (h / hh / L / l ...) */
-    /* 2. */
+    if -1
+        continue ;
+    else if h
+    else if hh
+    else if l
+    else ?
 
+    /* 2. puis la precision (par défaut si rien n'est donné par l'user) */
+    if -1
+        precision par defaut : 1
+    else 
+
+    /* 3. convertir le nbr to str */
+
+    /* 4. puis la largeur */
+
+    /* 5. puis les options ( - / + / 0 / ' ' / #) */
+
+    /* 6. on commit le change dans le buffer de l'element de LL */
+    data->buffer = str;
+    free (str); // ??cela risque t il de supprimer notre str fraichement assemblée et rangeee dans le buffer de la LL
 }
