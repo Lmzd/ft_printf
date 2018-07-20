4bd9072524a7e01bb3c44d69617ea843dde41ff4
Paul BLOUIN <pblouin@e3r7p20.42.fr>
2018-07-20 16:49:51 +0200

    issues on width printer

--- srcs/format/formatters/ft_format_int.c
+++ srcs/format/formatters/ft_format_int.c
@@ -6,7 +6,7 @@
 /*   By: pblouin <pblouin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/20 14:39:09 by lmazeaud          #+#    #+#             */
-/*   Updated: 2018/07/20 16:37:48 by pblouin          ###   ########.fr       */
+/*   Updated: 2018/07/20 16:48:28 by pblouin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,18 @@ void    ft_format_int_modifier(t_data *elem, va_list ap)
     {
         modifier.h = 0;
         value = (char)va_arg(ap, int);
-    }
-    if (modifier.h)
+    } else if (modifier.h)
         value = (short)va_arg(ap, int);
-    if (modifier.l)
+    else if (modifier.l)
         value = va_arg(ap, long int);
-    if (modifier.ll)
+    else if (modifier.ll)
         value = va_arg(ap, long long int);
-    if (modifier.j)
+    else if (modifier.j)
         value = va_arg(ap, intmax_t);
-    if (modifier.z)
+    else if (modifier.z)
         value = va_arg(ap, size_t);
-        
+    else
+        value = va_arg(ap, int);
     elem->buffer = ft_itoa_intmax(value);
 }
 
