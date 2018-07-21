29139f24e3525e862a524f21f18db9f8359191b9
Louis MAZEAUD <lmazeaud@e3r12p18.42.fr>
2018-07-21 22:02:03 +0200

    add moins flag handler

---
+++ srcs/format/ft_format_moins_flag_handler.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_format_moins_flag_handler.c                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmazeaud <lmazeaud@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/21 17:07:37 by lmazeaud          #+#    #+#             */
+/*   Updated: 2018/07/21 20:23:27 by lmazeaud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/ft_printf.h"
+
+void    ft_format_moins_flag_handler(t_data *elem)
+{
+    char    *str;
+    char    *buf;
+    int     range;
+    int     len;
+    int     width;
+
+    width = elem->width;
+    if (width < 0 || !elem->flags.dash)
+        return ;
+    len = ft_strlen(elem->buffer);
+    buf = ft_strdup(elem->buffer);
+    str = ft_strnew(width - len);
+    range = width - len;
+    while (range--)
+        str[--width] = ' ';
+    while (len--)
+        str[--width] = buf[len];
+    elem->buffer = str;
+    free(str);
+}
