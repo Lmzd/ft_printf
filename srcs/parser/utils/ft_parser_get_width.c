72d3189f6dc150e42b945b3a9ecfe16e2831e14a
Louis MAZEAUD <lmazeaud@e3r11p15.42.fr>
2018-07-22 16:06:25 +0200

    deals with precison number on getting the width

--- srcs/parser/utils/ft_parser_get_width.c
+++ srcs/parser/utils/ft_parser_get_width.c
@@ -6,7 +6,7 @@
 /*   By: lmazeaud <lmazeaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/17 18:21:43 by lmazeaud          #+#    #+#             */
-/*   Updated: 2018/07/19 11:09:02 by lmazeaud         ###   ########.fr       */
+/*   Updated: 2018/07/22 16:03:28 by lmazeaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,25 @@
 int		ft_parser_get_width(char *reg)
 {
 	int		i;
-	int 	len;
+	int 	res;
+    int     len;
+    char    *str;
 
     i = 0;
-    while (*reg <= 47 || *reg >= 58)
-        reg++;
-    len = ft_atoi(reg);
-	return ((!len) ? -1 : len);
+    len = 0;
+    while (reg[i] != '.' && reg[i++] != '\0')
+        len++;
+    i = 0;
+    if (len <= 1)
+        return (-1);
+    str = ft_strnew(len);
+    while (len--)
+    {
+        str[i] = reg[i];
+        i++;
+    }
+    while (*str <= 47 || *str >= 58)
+        str++;
+    res = ft_atoi(str);
+	return ((!res) ? -1 : res);
 }
