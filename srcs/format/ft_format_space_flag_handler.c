9fd7f96adaa9580e2397083af8bc73932363f753
Louis MAZEAUD <lmazeaud@e3r11p15.42.fr>
2018-07-23 00:17:13 +0200

    zero flag and space flag case

--- srcs/format/ft_format_space_flag_handler.c
+++ srcs/format/ft_format_space_flag_handler.c
@@ -6,7 +6,7 @@
 /*   By: lmazeaud <lmazeaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/21 18:36:51 by lmazeaud          #+#    #+#             */
-/*   Updated: 2018/07/21 19:08:55 by lmazeaud         ###   ########.fr       */
+/*   Updated: 2018/07/23 00:05:15 by lmazeaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,15 @@ void    ft_format_space_flag_handler(t_data *elem)
         return ;
     if (!elem->flags.spaces || elem->neg)
         return ;
+    buf = strdup(elem->buffer);
     if (elem->width < 0) 
     {
-        buf = strdup(elem->buffer);
         len = ft_strlen(buf);
         str = ft_strnew(len + 2);
         str[0] = ' ';
         elem->buffer = ft_strcat(str, buf);
+    } else {
+        buf[0] = ' ';
+        elem->buffer = ft_strdup(buf); 
     }
 }
