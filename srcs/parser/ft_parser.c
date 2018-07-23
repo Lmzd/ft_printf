7345f7813f86b9a8541e06c28e6f36d32da80957
Louis MAZEAUD <lmazeaud@e3r9p13.42.fr>
2018-07-23 20:13:48 +0200

    clarify parser main function

--- srcs/parser/ft_parser.c
+++ srcs/parser/ft_parser.c
@@ -6,7 +6,7 @@
 /*   By: lmazeaud <lmazeaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/09 17:58:51 by lmazeaud          #+#    #+#             */
-/*   Updated: 2018/07/22 20:44:24 by lmazeaud         ###   ########.fr       */
+/*   Updated: 2018/07/23 19:47:27 by lmazeaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,20 +24,17 @@ t_data		*ft_parser(const char *format)
     t_data	*begin_list;
 	t_data	*curr;
 
-    str_mem = str;
-	str = ft_strdup(format);
     curr = NULL;
 	begin_list = curr;
-	while (*str)
+	while (*format)
 	{
 		curr = ft_list_create_data_elem();
-		ft_parser_get_text_and_expression(curr, &str);
+		ft_parser_get_text_and_expression(curr, (char**) &format);
 		if (curr->expression != NULL)
 			ft_parser_options_checker(curr);
 		else
             ft_parser_put_default(curr);
 		ft_list_append(&begin_list, curr);
 	}
-    free(str_mem);
 	return(begin_list);
 }
